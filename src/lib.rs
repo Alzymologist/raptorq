db441346cb1f08b8a819e40ffb548079d4c1bb1a
Vera Abramova <abramova.vera@gmail.com>
2023-01-24 12:52:35 +0200

    fix: features checker

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(clippy::needless_return, clippy::unreadable_literal)]
 #![no_std]
-#![cfg(any(feature = "std", feature = "metal"))]
 
 #[cfg(feature = "metal")]
 #[macro_use]
@@ -13,43 +12,81 @@ extern crate core;
 #[macro_use]
 extern crate std;
 
+#[cfg(any(feature = "std", feature = "metal"))]
 mod arraymap;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod base;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod constraint_matrix;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod decoder;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod encoder;
+mod features_check;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod gf2;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod graph;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod iterators;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod matrix;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod octet;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod octet_matrix;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod octets;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod operation_vector;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod pi_solver;
 #[cfg(feature = "python")]
 mod python;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod rng;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod sparse_matrix;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod sparse_vec;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod symbol;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod systematic_constants;
+#[cfg(any(feature = "std", feature = "metal"))]
 mod util;
 #[cfg(feature = "wasm")]
 mod wasm;
 
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::base::partition;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::base::EncodingPacket;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::base::ObjectTransmissionInformation;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::base::PayloadId;
-#[cfg(not(any(feature = "python", feature = "wasm")))]
+#[cfg(all(
+    any(feature = "std", feature = "metal"),
+    not(feature = "python"),
+    not(feature = "wasm")
+))]
 pub use crate::decoder::Decoder;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::decoder::SourceBlockDecoder;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::encoder::calculate_block_offsets;
-#[cfg(not(any(feature = "python", feature = "wasm")))]
+#[cfg(all(
+    any(feature = "std", feature = "metal"),
+    not(feature = "python"),
+    not(feature = "wasm")
+))]
 pub use crate::encoder::Encoder;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::encoder::EncoderBuilder;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::encoder::SourceBlockEncoder;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::encoder::SourceBlockEncodingPlan;
 #[cfg(feature = "python")]
 pub use crate::python::raptorq;
@@ -57,6 +94,7 @@ pub use crate::python::raptorq;
 pub use crate::python::Decoder;
 #[cfg(feature = "python")]
 pub use crate::python::Encoder;
+#[cfg(any(feature = "std", feature = "metal"))]
 pub use crate::systematic_constants::extended_source_block_symbols;
 #[cfg(feature = "wasm")]
 pub use crate::wasm::Decoder as WasmDecoder;
