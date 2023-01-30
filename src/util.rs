115c69807bddc826c2685975045710382a3cd09c
Vera Abramova <abramova.vera@gmail.com>
2023-01-30 12:49:22 +0200

    feat: remove conversion to f64, fix features

--- src/util.rs
+++ src/util.rs
@@ -31,3 +31,12 @@ pub fn get_both_indices<T>(vector: &mut [T], i: usize, j: usize) -> (&mut T, &mu
         return (&mut last[0], &mut first[j]);
     }
 }
+
+// This should eventually become <https://doc.rust-lang.org/std/primitive.u64.html#method.div_ceil>
+// when stabilized.
+// The result known to not overflow from elsewhere.
+pub fn int_div_ceil(num: u64, denom: u64) -> u32 {
+    if denom==0 {panic!("called int_div_ceil on {} {}", num, denom)}
+    if num%denom == 0 {(num/denom) as u32}
+    else {(num/denom  + 1) as u32}
+}
